ee98d39510acf5b52940cf018c8326b8da739283
Sebastian <95112798+sjaegervigo@users.noreply.github.com>
2025-10-09 01:24:28 -0500

    Working on the main class, I instanciate 3 global variables, for Scanner, LeaveSystem and AuthService. Then new data is loaded when the program is started, then the program will ask you for a rol between Employee or Manager, if null exit the main loop, then I call the login function where I will use the auth Service.

--- src/main/java/org/example/leavesystem/Main.java
+++ src/main/java/org/example/leavesystem/Main.java
@@ -7,7 +7,52 @@ import java.util.Scanner;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
 
+    private static final LeaveSystem leaveSystem = LeaveSystem.getInstance();
+    private static final Scanner sc = new Scanner(System.in);
+    private static final AuthService authService = new AuthService(leaveSystem);
+
     public static void main(String[] args){
-        System.out.println("Hola");
+        loadData();
+
+        do{
+            String role = askForRol();
+            if(role == null){
+                System.out.println("Goodbye");
+                break;
+            }
+
+            LoginResult result = login(role);
+        } while (role != null);
     }
+
+    public static LoginResult login(String role){
+        return new LoginResult(false, null);
+    }
+
+    public static void loadData(){
+        authService.leaveSystem.addEmployee(new Employee("Sebastian", "sjaegervigo@gmail.com", "admin"));
+        authService.leaveSystem.addManager(new Manager("Alejandro", "ajaegervigo@gmail.com", "admin"));
+    }
+
+    public static String askForRol(){
+        System.out.println("Who are you?");
+        System.out.println("1. Employee");
+        System.out.println("2. Manager");
+        System.out.println("-------------");
+        System.out.println("0. Exit");
+        int option = Integer.parseInt(sc.nextLine());
+
+        return switch (option){
+            case 1 -> "Employee";
+            case 2 -> "Manager";
+            case 0 -> null;
+            default -> {
+                System.out.println("Invalid option");
+                yield askForRol();
+            }
+        };
+
+    }
+
+
 }

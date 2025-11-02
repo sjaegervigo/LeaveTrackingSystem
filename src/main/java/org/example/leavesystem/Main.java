953842dc7b4672f303b55221657ce7c076b72684
sjaegervigo <sjaegervigo@gmail.com>
2025-11-01 22:16:52 -0500

    creating showEmployeeMenu

--- src/main/java/org/example/leavesystem/Main.java
+++ src/main/java/org/example/leavesystem/Main.java
@@ -37,7 +37,43 @@ public class Main {
     }
 
     public static void showEmployeeMenu(Employee logedEmployee){
-        System.out.println();
+        System.out.println("What do you want to do now?\n");
+        System.out.println("1. View my leave requests");
+        System.out.println("2. Create new leave request");
+        try{
+            int userOption = Integer.parseInt(sc.nextLine());
+            switch (userOption){
+                case 1 -> System.out.println(logedEmployee.getLeaveRequests());
+                case 2 -> {
+                    System.out.println("What kind of leave request do you want to create?");
+                    System.out.println("1. Vacation request");
+                    System.out.println("2. Medical request");
+                    System.out.println("3. Emergency request");
+                    userOption = Integer.parseInt(sc.nextLine());
+                    switch (userOption){
+                        case 1 -> {
+                            System.out.println("Enter the start date: ");
+                            String startDate = sc.nextLine();
+                            System.out.println("Enter the end date: ");
+                            String endDate = sc.nextLine();
+                            VacationsRequest vr = new VacationsRequest(logedEmployee, LocalDate.parse(startDate), LocalDate.parse(endDate));
+                            logedEmployee.getLeaveRequests().add(vr);
+                            leaveSystem.addLeaveRequest(vr);
+                        }
+                    }
+                }
+            }
+
+        } catch (NumberFormatException e){
+            System.out.println( "\n++++++++++++++++++++++++++++++++++++++\n"+
+                                "++ Exception catched!!              ++\n"+
+                                "++ Cause:                           ++\n"+
+                                "++ "+e.getMessage()+   "            ++\n"+
+                                "++++++++++++++++++++++++++++++++++++++\n");
+
+        }
+
+
     }
 
     public static LoginResult login(String role){
@@ -78,8 +114,6 @@ public class Main {
                                 "++ "+e.getMessage()+   "            ++\n"+
                                 "++++++++++++++++++++++++++++++++++++++\n");
             askForRol();
-        } finally {
-
         }
         return null;
     }
